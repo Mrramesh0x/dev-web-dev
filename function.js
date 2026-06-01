8e6588628dc12d2c2b2fd8ef48019aeccec9de8e
Mrramesh <rameshside67@gmail.com>
2026-06-01 11:53:41 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,28 @@
+function goodMorning(){
+alert("Good Morning Dev")
+}
+
+function goodAfternoon(){
+alert("Good afternoon Dev")
+}
+
+function goodEvening(){
+alert("Good evening Dev")
+}
+
+function goodNight(){
+alert("Good night Dev")
+}
+
+
+const goodMorningButton = document.querySelector(".morning-btn")
+const goodAfternoonButton = document.querySelector(".afternoon-btn")
+const goodEveningButton = document.querySelector(".evening-btn")
+const goodNightButton = document.querySelector(".night-btn")
+
+goodMorningButton.addEventListener("click",goodMorning)
+goodAfternoonButton.addEventListener("click",goodAfternoon)
+goodEveningButton.addEventListener("click",goodEvening)
+goodNightButton.addEventListener("click",goodNight)
+
+
---
+++ script.js
@@ -0,0 +1,27 @@
+// const getNameButton = document.querySelector(".get-name")
+// // document.getElementsByClassName(".get-name")
+// // document.getElementById("n")
+// const name = "dev"
+// console.log(getNameButton)
+// const getFunc = () => {
+//     console.log(name)
+// }
+// // function namefunc(){
+// // console.log("function work")
+// // }
+
+// // namefunc() 
+// getNameButton.addEventListener("click",()=>{
+//     alert(name)
+// })
+
+function name(){ // simple function
+    console.log("name")
+}
+
+const name1 = () => { // arrow function
+console.log("name 1")
+}
+
+name()
+name1()
